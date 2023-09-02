307e55ccb8424290d9194147c9d7d71b056240c3
Rune T. Larsen <rune@hjemmekino.no>
2023-09-02 09:19:19 +0200

    Comparison update

---
+++ src/evkxapi/Helpers/EvConversion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evkxapi.Helpers
+{
+    public static class EvConversion
+    {
+
+        public static decimal? ToMilesFromKph(decimal? km)
+        {
+            if (km != null)
+            {
+                return Decimal.Divide(km.Value, (decimal)1.609);
+            }
+
+            return null;
+        }
+
+        public static decimal? ToKmFromMiles(decimal? miles)
+        {
+            if (miles != null)
+            {
+                return Decimal.Multiply(miles.Value, (decimal)1.609);
+            }
+
+            return null;
+        }
+
+        public static decimal? ToMiKwhFromKwh100km(decimal? consumption)
+        {
+            decimal milesDriven = Decimal.Divide(100, (decimal)1.609);
+
+            if (consumption != null)
+            {
+                return Decimal.Divide(milesDriven, consumption.Value);
+            }
+
+            return null;
+        }
+
+
+    }
+}
