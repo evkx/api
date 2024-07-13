dfab761f879fc8ff94bc1254a327462906d7d3a8
Rune T. Larsen <rune@hjemmekino.no>
2024-07-13 17:47:41 +0200

    Deleted old props

--- src/evkx.models/Models/Seatoption.cs
+++ src/evkx.models/Models/Seatoption.cs
@@ -13,30 +13,6 @@ namespace evdb.Models
         public Seatoption()
         {
             SeatCategory = models.Enums.SeatCategory.None;
-            ElectricAdjustment = new EVFeature();
-            ElectricLumbarAdjustment = new EVFeature();
-            Massage = new EVFeature();
-            Ventilation = new EVFeature();
-            SeatHeating = new EVFeature();
-            MemoryDriverSeat = new EVFeature();
-            MemoryPassangerSeat = new EVFeature();
-            IntegratedHeadrest = new EVFeature();
-            HeightAdjustableHeadrest = new EVFeature();
-            LengthAdjustableHeadrest = new EVFeature();
-            AdjustableThighSupport = new EVFeature();
-            ReclineAdjustment = new EVFeature();
-            ElectricAdjustableThighSupport = new EVFeature();
-            AdjustableSideSupportBack = new EVFeature();
-            AdjustableSideSupportBottom = new EVFeature();
-            EasyAccessDriverSeat = new EVFeature();
-            EasyAccessPassenger = new EVFeature();
-            FootrestPassenger = new EVFeature();
-            LegSupportPassenger = new EVFeature();
-            Foldable = new EVFeature();
-            ElectricFoldable = new EVFeature();
-            ForeAndAftAdjustment = new EVFeature();
-            SeatCushionAngleAdjustment = new EVFeature();
-            HeightAdjustment = new EVFeature();
         }
 
         public bool? Standard { get; set; }
@@ -48,63 +24,11 @@ namespace evdb.Models
         public SeatCategory? SeatCategory { get; set; }
 
         public string? SeatSplit { get; set; }
-
-        public EVFeature? ForeAndAftAdjustment { get; set; }
-
-        public EVFeature? ReclineAdjustment { get; set; }
-
-        public EVFeature? HeightAdjustment { get; set; }
-
-        public EVFeature? SeatCushionAngleAdjustment { get; set; }
-
-        public EVFeature? ElectricLumbarAdjustment { get; set; }
-
-        public EVFeature? HeightAdjustableHeadrest { get; set; }
-
-        public EVFeature? LengthAdjustableHeadrest { get; set; }
-
-        public EVFeature? IntegratedHeadrest { get; set; }
-
-        public EVFeature? AdjustableThighSupport { get; set; }
-
-        public EVFeature? ElectricAdjustableThighSupport { get; set; }
-
-        public EVFeature? AdjustableSideSupportBack { get; set; }
-
-        public EVFeature? AdjustableSideSupportBottom { get; set; }
-
-        public EVFeature? ElectricAdjustment { get; set; }
-
-        public EVFeature? ElectricAdjustmentPassenger { get; set; }
-
-        public EVFeature? MemoryDriverSeat { get; set; }
-
-        public EVFeature? MemoryPassangerSeat { get; set; }
-
-        public EVFeature? Massage { get; set; }
-
-        public EVFeature? Ventilation { get; set; }
-
-        public EVFeature? SeatHeating { get; set; }
-       
-        public EVFeature? EasyAccessDriverSeat { get; set; }
-
-        public EVFeature? EasyAccessPassenger { get; set; }
-
-        public EVFeature? FootrestPassenger { get; set; }
-
-        public EVFeature? LegSupportPassenger { get; set; }
-
-        public EVFeature? Foldable { get; set; }
-
-        public EVFeature? ElectricFoldable { get; set; }
-
+        
         /// <summary>
         /// The seats for this seat option
         /// </summary>
         public List<Seat>? Seat { get; set; }
-
-
 
         public SeatRowFeatureStatus HasForeAndAftAdjustment()
         {
